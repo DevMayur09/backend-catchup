912319129c6da2c540864572273d1c31a31a508f
mayur <mayur.thool@viklele.com>
2024-10-23 23:18:42 +0530

    implement some validation rule on fields

--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,53 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstname: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8, // Use `minlength` for Mongoose validation
+      maxlength: 16, // Use `maxlength` for Mongoose validation
+      validate: {
+        validator: function (value) {
+          const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,16}$/; // Added max length check in regex
+          return regex.test(value);
+        },
+        message:
+          "Invalid password! Password must be 8-16 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character.",
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate: {
+        validator: function (value) {
+          if (["male", "female", "other"].includes(value)) return true;
+        },
+        message: "Invalid gender.",
+      },
+    },
+    hobbies: { type: [String] },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
