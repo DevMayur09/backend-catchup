b4873f6b44cbe0e2c1d84f4d88654a61335f3017
mayur <mayur.thool@viklele.com>
2024-10-23 21:34:49 +0530

    implement simple crud on USER collection

--- src/app.js
+++ src/app.js
@@ -3,15 +3,58 @@ const connectDB = require("./config/database.js");
 const User = require("./models/user.js");
 const app = express();
 const { adminAuth, userAuth } = require("./middlewares/auth");
+const user = require("./models/user.js");
 
 const PORT = 3000;
 app.use(express.json());
 
+// POST /signup for user creation
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
 
   await user.save();
   res.send("User Added Successfully !!!");
+});
+
+// GET /feed for gettings feeds
+app.get("/feeds", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Something went wrong...");
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const emailId = req.body.emailId;
+  try {
+    const user = await User.findOne({ emailId: emailId });
+    res.send(user);
+  } catch (error) {
+    res.status(400).send("Something went wrong...");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (error) {
+    res.status(400).send("Something went wrong...");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, { returnDocument: "after" });
+    res.send("user updated successfully");
+  } catch (error) {
+    res.status(400).send("Something went wrong...");
+  }
 });
 
 connectDB()
