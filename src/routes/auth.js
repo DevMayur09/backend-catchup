7c7d4acfc45ea8a6e4c92ecc746d254dc7dc771b
mayur <mayur.thool@viklele.com>
2024-10-26 15:50:26 +0530

    minor changes in auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -58,6 +58,8 @@ authRouter.post("/logout", async (req, res) => {
   res.cookie("token", null, {
     expires: new Date(Date.now()),
   });
+
+  res.send("Logout Success");
 });
 
 module.exports = authRouter;
