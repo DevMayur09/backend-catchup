49a25205c8c3cd3bca5e66962cb5a16ade341f6e
mayur <mayur.thool@viklele.com>
2024-10-26 15:52:24 +0530

    PATCH /profile/edit end-point implemented

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,9 +2,10 @@ const express = require("express");
 const profileRouter = express.Router();
 
 const { userAuth } = require("../middlewares/auth");
+const { throwError, validateEditProfileData } = require("../utils/validation");
 
-// GET /profile
-profileRouter.get("/profile", userAuth, async (req, res) => {
+// GET /profile/view
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
     const userName = user.firstName;
@@ -15,4 +16,25 @@ profileRouter.get("/profile", userAuth, async (req, res) => {
   }
 });
 
+// PATCH /profile/edit
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req)) throwError("Invalid edit request");
+
+    const loggedInUser = req.user;
+
+    Object.keys(req.body).forEach((key) => {
+      loggedInUser[key] = req.body[key];
+    });
+
+    await loggedInUser.save();
+
+    res.json({
+      message: loggedInUser.firstName + " your profile updated successfully",
+      data: loggedInUser,
+    });
+  } catch (error) {
+    res.status(400).send(`Error: ${error.message}`);
+  }
+});
 module.exports = profileRouter;
