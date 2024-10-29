864bdad40711eb6653775407ed33ab9e2d9bd24e
mayur <mayur.thool@viklele.com>
2024-10-30 01:04:17 +0530

    user router implemented in app

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/auth.js");
 const profileRouter = require("./routes/profile.js");
 const requestRouter = require("./routes/request.js");
+const userRouter = require("./routes/user.js");
 
 app.use(express.json());
 app.use(cookieParser());
@@ -14,7 +15,7 @@ app.use(cookieParser());
 app.use(authRouter);
 app.use(profileRouter);
 app.use(requestRouter);
-
+app.use(userRouter);
 const PORT = 3000;
 
 connectDB()
