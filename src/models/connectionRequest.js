0724b762cf91b3bc6386611e48a1108d02a5624c
mayur <mayur.thool@viklele.com>
2024-10-29 14:06:40 +0530

    compound indexes added for fromUserId and toUserId in connectionrequest

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -5,12 +5,10 @@ const connectionRequestSchema = new mongoose.Schema(
   {
     fromUserId: {
       type: mongoose.Schema.ObjectId,
-      index: true,
       required: true,
     },
     toUserId: {
       type: mongoose.Schema.ObjectId,
-      index: true,
       required: true,
     },
     status: {
@@ -26,6 +24,10 @@ const connectionRequestSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+//Compound index so query with find becomes very fast....
+
+connectionRequestSchema.index({ fromUserId: 1 }, { toUserId: 1 });
 
 connectionRequestSchema.pre("save", function (next) {
   const connectionRequest = this;
